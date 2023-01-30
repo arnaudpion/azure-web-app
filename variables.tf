80491cfa74d2a23df8404fad8c7ca835ed324777
Arnaud Pion <apion@paloaltonetworks.com>
2023-01-30 21:47:25 +0100

    Added min_tls_version variable

--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "location" {
   type        = string
   description = "The Azure location where all resources in this example should be created"
 }
+
+variable "min_tls_version" {
+  type        = string
+  description = "Minimum TLS version supported"
+  default     = "1.2"
+}
